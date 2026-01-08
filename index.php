52b2678d3787f6aab96079e54d6b04769665603b
Akanksha1790 <chauhanakansha629@gmail.com>
2026-01-09 01:21:25 +0530

    improve index structure

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "<h1>Hello from my first WordPress theme</h1>";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First WordPress Theme</title>
+</head>
+<body>
+    <h1>Welcome to my WordPress Theme</h1>
+    <p>This theme is created for learning purposes.</p>
+</body>
+</html>
+
